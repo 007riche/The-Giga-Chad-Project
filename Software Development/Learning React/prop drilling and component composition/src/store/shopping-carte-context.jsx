8afeded99c8aa2f2f349aa68a59fa906d80f9b08
007riche <61353180+007riche@users.noreply.github.com>
2026-04-27 16:28:53 +0200

    Feat(Learning react/ project management app): end of the introduction to useReducer hook

--- Software Development/Learning React/prop drilling and component composition/src/store/shopping-carte-context.jsx
+++ Software Development/Learning React/prop drilling and component composition/src/store/shopping-carte-context.jsx
@@ -17,7 +17,61 @@ export const CartContext = createContext({
 });
 
 // The callBack the reducer has to use 
-function shoppingCartReducer(state, action) { return state; }
+function shoppingCartReducer(state, action) {
+    if (action.type === 'ADD_ITEM') {
+        const updatedItems = [...state.items];
+
+        const existingCartItemIndex = updatedItems.findIndex(
+            (cartItem) => cartItem.id === action.payload
+        );
+        const existingCartItem = updatedItems[existingCartItemIndex];
+
+        if (existingCartItem) {
+            const updatedItem = {
+                ...existingCartItem,
+                quantity: existingCartItem.quantity + 1,
+            };
+            updatedItems[existingCartItemIndex] = updatedItem;
+        } else {
+            const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
+            updatedItems.push({
+                id: action.payload,
+                name: product.title,
+                price: product.price,
+                quantity: 1,
+            });
+        }
+
+        return {
+            items: updatedItems,
+        };
+    }
+
+    if (action.type === 'UPDATE_ITEM') {
+        const updatedItems = [...state.items];
+        const updatedItemIndex = updatedItems.findIndex(
+            (item) => item.id === action.payload.productId
+        );
+
+        const updatedItem = {
+            ...updatedItems[updatedItemIndex],
+        };
+
+        updatedItem.quantity += action.payload.amount;
+
+        if (updatedItem.quantity <= 0) {
+            updatedItems.splice(updatedItemIndex, 1);
+        } else {
+            updatedItems[updatedItemIndex] = updatedItem;
+        }
+
+        return {
+            ...state,
+            items: updatedItems,
+        };
+    }
+    return state;
+}
 
 export default function CartContextProvider({ children }) {
     // using reducer hook to manage the state
@@ -88,12 +142,30 @@ export default function CartContextProvider({ children }) {
         });
     }
 
-    const ctxValue = {
-        items: shoppingCart.items,
-        // items: shoppingCartReducerState.items,
+    function handleAddItemToCartThroughReducer(id) {
+        shoppingCartDispatch({
+            type: 'ADD_ITEM',
+            payload: id
+        });
 
-        addItemToCart: handleAddItemToCart,
-        updateItemQuantity: handleUpdateCartItemQuantity,
+    }
+
+    function handleUpdateCartItemQuantityThroughReducer(productId, amount) {
+        shoppingCartDispatch({
+            type: "UPDATE_ITEM",
+            payload: {
+                productId,
+                amount
+            }
+        });
+    }
+
+    const ctxValue = {
+        // items: shoppingCart.items,
+        items: shoppingCartReducerState.items,
+
+        addItemToCart: handleAddItemToCartThroughReducer,
+        updateItemQuantity: handleUpdateCartItemQuantityThroughReducer,
     };
 
     return <CartContext value={ctxValue}>
