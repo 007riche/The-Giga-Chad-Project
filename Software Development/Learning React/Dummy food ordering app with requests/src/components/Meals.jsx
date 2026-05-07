c16d59451afed94ecc6e64732698e612900921b5
007riche <61353180+007riche@users.noreply.github.com>
2026-05-07 18:11:08 +0200

    Feat(Learning React/ Food Order App): Error message when failed requests happen

--- Software Development/Learning React/Dummy food ordering app with requests/src/components/Meals.jsx
+++ Software Development/Learning React/Dummy food ordering app with requests/src/components/Meals.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import MealItem from "./MealItems";
 import useHttp from "../hooks/useHttp";
+import Error from "./ErrorPage";
 
 // Outside of the component function to avoid recreating it as a new object 
 // each time the com fx re-renders (that usually create infinite loops)
@@ -13,7 +14,11 @@ export default function Meals() {
     } = useHttp('http://localhost:3000/meals', requestConfig, []);
 
     if (isLoading) {
-        return <p>Fecthing data, please have patience...</p>
+        return <p className="center">Fecthing data, please have patience...</p>
+    }
+
+    if (error) {
+        return <Error title="Failed to fetch meals" message={error} />
     }
 
     return (
