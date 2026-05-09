6325c8e6fa222f0c51da892ca9f8459dc018f8c0
007riche <61353180+007riche@users.noreply.github.com>
2026-05-09 18:09:58 +0200

    Feat(Learning React/ Food Order App): intro to redux in react app

--- Software Development/Learning React/Introduction to redux/src/index.js
+++ Software Development/Learning React/Introduction to redux/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+// The Provider from the react-redux
+// It has a store prop to be set
+// Should wrap as highest component as possible
+// in the component tree 
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 
+// Our store
+import store from './store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
