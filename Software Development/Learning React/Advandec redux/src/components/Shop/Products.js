71f1e91e321a081ccb030b489320fb9333b859a8
007riche <61353180+007riche@users.noreply.github.com>
2026-05-11 17:01:05 +0200

    Feat(Learning React/ Food Order App): Refresher redux with redux toolkit

--- Software Development/Learning React/Advandec redux/src/components/Shop/Products.js
+++ Software Development/Learning React/Advandec redux/src/components/Shop/Products.js
@@ -1,16 +1,36 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_DATA = [
+  {
+    id: 'p1',
+    price: 6,
+    title: 'My First Book',
+    description: 'the first book ever written',
+  },
+  {
+    id: 'p2',
+    price: 4,
+    title: 'My Second Book',
+    description: 'the second book ever written',
+  },
+];
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_DATA.map((product) => (
+          <ProductItem
+            // Key, important
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
---
+++ Software Development/Learning React/Advandec redux/src/store/cart-slice.js
@@ -0,0 +1,48 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const intialCartState = {
+    items: [],
+    totalQuantity: 0,
+};
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState: intialCartState,
+    reducers: {
+        addItemSingleItemToCart(state, action) {
+            const newItem = action.payload;
+            state.totalQuantity++;
+            const existingItem = state.items.find((item) => item.id === newItem.id);
+            if (!existingItem) {
+                // Mutations of the existing state, but fine
+                // Redux-toolkit gots us covered
+                state.items.push({
+                    id: newItem.id,
+                    price: newItem.price,
+                    quantity: 1,
+                    totalPrice: newItem.price,
+                    name: newItem.title
+                });
+            } else {
+                // Mutations of the existing state, but fine
+                // Redux-toolkit gots us covered
+                existingItem.quantity++;
+                existingItem.totalPrice = existingItem.totalPrice + newItem.price;
+            }
+        },
+        removeItemFromCart(state, action) {
+            const id = action.payload;
+            state.totalQuantity--;
+            const existingItem = state.items.find((item) => item.id === id);
+            if (existingItem.quantity === 1) {
+                state.items = state.items.filter(item => item.id !== id);
+            } else {
+                existingItem.quantity--;
+                existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
+            }
+        }
+    }
+});
+
+export const cartActions = cartSlice.actions;
+export default cartSlice;
