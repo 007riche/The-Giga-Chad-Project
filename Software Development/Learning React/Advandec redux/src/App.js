7b0d22ee8e1b66d3f8b37c5dc07846e2b8308055
007riche <61353180+007riche@users.noreply.github.com>
2026-05-12 18:16:24 +0200

    Feat(Learning React/ Food Order App): WIP feedback

--- Software Development/Learning React/Advandec redux/src/App.js
+++ Software Development/Learning React/Advandec redux/src/App.js
@@ -1,4 +1,4 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -7,6 +7,7 @@ import Products from './components/Shop/Products';
 // import 'dotenv/config'; // not working
 import { useEffect } from 'react';
 import { DB_LINK } from './environ';
+import { uiActions } from './store/ui-slice';
 
 // DB link
 
@@ -14,17 +15,30 @@ import { DB_LINK } from './environ';
 const CART_DB_LINK = DB_LINK + 'cart.json';
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector(globalStore => globalStore.ui.cartIsVisible);
   const cart = useSelector(globalStore => globalStore.cart);
 
   // side effect out side of the store, to keep the store clean,
   // to avoid breaking immutability of the global store
   useEffect(() => {
-    fetch(
-      CART_DB_LINK, {
-      method: 'PUT',
-      body: JSON.stringify(cart),
-    });
+    const sendCartData = async () => {
+      dispatch(uiActions.showNotification({
+
+      }));
+      const response = fetch(
+        CART_DB_LINK, {
+        method: 'PUT',
+        body: JSON.stringify(cart),
+      });
+
+      if (!response.ok) {
+        throw new Error("Something went wrong when sending data.");
+
+      }
+
+      const responseData = (await response).json();
+    }
   }, [cart]);
 
   return (
