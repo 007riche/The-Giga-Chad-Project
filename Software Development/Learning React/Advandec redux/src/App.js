6292394f224b42ce30a9d0389398b08e535ac6df
007riche <61353180+007riche@users.noreply.github.com>
2026-05-12 17:53:14 +0200

    Feat(Learning React/ Food Order App): WIP, Dummy db, env manipulation b

--- Software Development/Learning React/Advandec redux/src/App.js
+++ Software Development/Learning React/Advandec redux/src/App.js
@@ -3,8 +3,28 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
+// For manipulating env content
+import 'dotenv/config';
+import { useEffect } from 'react';
+
+// DB link
+
+const DB_LINK = process.env.DB_LINK;
+const CART_DB_LINK = DB_LINK + 'cart.json';
+
 function App() {
-  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const showCart = useSelector(globalStore => globalStore.ui.cartIsVisible);
+  const cart = useSelector(globalStore => globalStore.cart);
+
+  // side effect out side of the store, to keep the store clean,
+  // to avoid breaking immutability of the global store
+  useEffect(() => {
+    fetch(
+      CART_DB_LINK, {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
 
   return (
     <Layout>
