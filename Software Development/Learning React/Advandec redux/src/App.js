1605b5e072695ce1705b50ee29e78104192c30a4
007riche <61353180+007riche@users.noreply.github.com>
2026-05-13 10:16:00 +0200

    Fix and Feat(Learning React/ Food Order App): Found one way around env, finished notification feedback

--- Software Development/Learning React/Advandec redux/src/App.js
+++ Software Development/Learning React/Advandec redux/src/App.js
@@ -2,10 +2,11 @@ import { useDispatch, useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification/Notification';
 
 // For manipulating env content
 // import 'dotenv/config'; // not working
-import { useEffect } from 'react';
+import { Fragment, useEffect } from 'react';
 import { DB_LINK } from './environ';
 import { uiActions } from './store/ui-slice';
 
@@ -14,19 +15,24 @@ import { uiActions } from './store/ui-slice';
 // const DB_LINK = process.env.DB_LINK;
 const CART_DB_LINK = DB_LINK + 'cart.json';
 
+let isInitialLoading = true;
+
 function App() {
   const dispatch = useDispatch();
   const showCart = useSelector(globalStore => globalStore.ui.cartIsVisible);
   const cart = useSelector(globalStore => globalStore.cart);
+  const notification = useSelector(globalStore => globalStore.ui.notification);
 
   // side effect out side of the store, to keep the store clean,
   // to avoid breaking immutability of the global store
   useEffect(() => {
     const sendCartData = async () => {
       dispatch(uiActions.showNotification({
-
+        status: 'pending',
+        title: 'Sending',
+        message: 'Sending cart data',
       }));
-      const response = fetch(
+      const response = await fetch(
         CART_DB_LINK, {
         method: 'PUT',
         body: JSON.stringify(cart),
@@ -34,18 +40,41 @@ function App() {
 
       if (!response.ok) {
         throw new Error("Something went wrong when sending data.");
-
       }
 
-      const responseData = (await response).json();
+      // const responseData = response.json();
+      dispatch(uiActions.showNotification({
+        status: 'success',
+        title: 'Success',
+        message: 'Sent cart data successfully',
+      }));
     }
-  }, [cart]);
+
+    if (isInitialLoading) {
+      isInitialLoading = false;
+      return;
+    }
+    sendCartData().catch((error) => {
+      dispatch(uiActions.showNotification({
+        status: 'error',
+        title: 'Error',
+        message: 'Sending cart data failed',
+      }));
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {showCart && <Cart />};
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && <Notification
+        status={notification.status}
+        title={notification.title}
+        message={notification.message}
+      />}
+      <Layout>
+        {showCart && <Cart />};
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
