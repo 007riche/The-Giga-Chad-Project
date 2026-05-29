d9f681c48f28dbb2396e7c8a32cc5c172ca9db28
007riche <61353180+007riche@users.noreply.github.com>
2026-05-29 17:55:15 +0200

    Feat(Learning React/TanStack Query): useMutation hook

--- Software Development/Learning React/TanStack/src/components/Events/NewEvent.jsx
+++ Software Development/Learning React/TanStack/src/components/Events/NewEvent.jsx
@@ -2,24 +2,39 @@ import { Link, useNavigate } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import { useMutation } from '@tanstack/react-query';
+import { createNewEvent } from '../../util/http.js';
+import ErrorBlock from '../UI/ErrorBlock.jsx';
 
 export default function NewEvent() {
   const navigate = useNavigate();
-
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    mutationFn: createNewEvent,
+  }) // optimized for triggering changes in querie, 
+  // here used to send data
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
-      <EventForm onSubmit={handleSubmit}>
-        <>
+      <EventForm onSubmit={handleSubmit}>``
+        {isPending && 'Submitting...'}
+        {!isPending && (<>
           <Link to="../" className="button-text">
             Cancel
           </Link>
           <button type="submit" className="button">
             Create
           </button>
-        </>
+        </>)}
       </EventForm>
+      {isError &&
+        <ErrorBlock title="Failed to create event"
+          message={error.info?.message
+            || 'Failed to create event. Please ensure that you fill all the inputs and try later.'}
+        />
+      }
     </Modal>
   );
 }
