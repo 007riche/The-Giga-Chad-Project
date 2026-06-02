75c7e1017cb791d0ee9bbd60ede0325020ae40b0
007riche <61353180+007riche@users.noreply.github.com>
2026-06-02 15:34:42 +0200

    Feat(Learning React/TanStack Query): optimistic update, and handling its egde cases

--- Software Development/Learning React/TanStack/src/components/Events/EditEvent.jsx
+++ Software Development/Learning React/TanStack/src/components/Events/EditEvent.jsx
@@ -2,8 +2,8 @@ import { Link, useNavigate, useParams } from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
-import { useQuery } from '@tanstack/react-query';
-import { fetchEvent } from '../../util/http.js';
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { fetchEvent, queryClient, updateEvent } from '../../util/http.js';
 import LoadingIndicator from '../UI/LoadingIndicator.jsx';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
 
@@ -13,11 +13,42 @@ export default function EditEvent() {
   const params = useParams();
 
   const { data, isPending, isError, error } = useQuery({
-    queryKey: ['events', useParams.id], // cached results
+    queryKey: ['events', params.id], // cached results
     queryFn: ({ signal }) => fetchEvent({ signal, id: params.id })
   });
 
-  function handleSubmit(formData) { }
+  const { mutate } = useMutation({
+    mutationFn: updateEvent,
+    onMutate: async (data) => {
+      const newEvent = data.event;
+
+      await queryClient.cancelQueries({ queryKey: ['events', params.id], });
+      // Manually aborting, ongoing triggered queries by useQuery 
+      // to avoid clash update clashes
+      const previousEvent = queryClient.getQueriesData(['events', params.id]);
+      queryClient.setQueryData(['events', params.id], newEvent); // Optimistic and 
+      //  Manual update of 
+      // data identified by queryKey: ['events', params.id]
+      return { previousEvent }; // returns the previous state, accessed via the context
+      // usefull for rollback
+    },
+
+    // onError, something went wrong with the mutation and captured
+    onError: (error, data, context) => {
+      queryClient.setQueriesData(['events', params.id], context.previousEvent);
+    },
+
+    // utlimate process, final event
+    onSettled: () => {
+      queryClient.invalidateQueries(['events', params.id]); // mutation, 
+      // final event, error, rollback
+    }
+  });
+
+  function handleSubmit(formData) {
+    mutate({ id: params.id, event: formData });
+    navigate('../');
+  }
 
   function handleClose() {
     navigate('../');
