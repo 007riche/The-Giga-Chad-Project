d57daebdfa0f1ce80b955ff26c54f226912aa2ef
007riche <61353180+007riche@users.noreply.github.com>
2026-06-05 09:32:43 +0200

    Feat(Learning React/Next JS): sqlite with better-sqilte3, loading page and alternative way

--- Software Development/Learning React/Next dummy food app/app/meals/page.js
+++ Software Development/Learning React/Next dummy food app/app/meals/page.js
@@ -1,17 +1,33 @@
 import Link from "next/link";
+import classes from "./page.module.css"
+import MealsGrid from "../components/meals/meals-grid";
+import { getAllMeals } from "../lib/meals";
+import { Suspense } from "react";
 
-export default function Meals() {
+async function Meals() {
+    const meals = await getAllMeals();
+    return <MealsGrid meals={meals} />
+}
+
+export default async function MealsPage() {
     return (
-        <main>
-            <Link href="/">Home</Link>
-            <Link href="/meals/share">Community share</Link>
-            <ul>
-                <li>
-                    <Link href="/meals/burrata">Burrata</Link>
-                </li>
-                <Link href="/meals/carpaccio">Carpaccio</Link>
-                <li><Link href="/meals/pizza">Pizza</Link></li>
-            </ul>
-        </main>
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious meals, created {' '/*To print a whitespace between the 
+                    previous text and the span element*/}
+                    <span className={classes.highlight}>by you</span>
+                </h1>
+                <p>Choose your favourite recipe and cook it yourself. It is easy and fun!</p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">Share Your Favorite Recipe</Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <Suspense fallback={<p className={classes.loading}>Fetching meals...</p>}>
+                    <Meals />
+                </Suspense>
+            </main>
+        </>
     );
 }
--- Software Development/Learning React/Next dummy food app/notes.md
+++ Software Development/Learning React/Next dummy food app/notes.md
@@ -1,3 +1,8 @@
 By default, all the pages and components are rendered on the server, hence called React Server components(RSC)
 Any code that should execute (State, events, etc...) on the client side should explicitly be declared as a client side components.
 This can be done by using the  "use client" directive in that component being rendered on the client side.
+sqlite db pack: `better-sqlite3`
+
+#### Another reserved filname
+
+`loading`
