c220212adf24ba3640ab74a2fbdbae85277ec696
007riche <61353180+007riche@users.noreply.github.com>
2026-06-10 16:21:32 +0200

    Feat & Bug(Learning React/Next JS): refactoring, Typo error

---
+++ Software Development/Learning React/next-page-router-dummy-project/components/meetups/MeetupDetails.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react/jsx-runtime";
+
+function MeetupDetails(props) {
+    return (<Fragment>
+        <img
+            src={props.image}
+            alt={props.title}
+        />
+        <h1>{props.title}</h1>
+        <address>{props.address}</address>
+        <p>{props.description}</p>
+    </Fragment>);
+}
+
+export default MeetupDetails;
---
+++ Software Development/Learning React/next-page-router-dummy-project/pages/[meetupId]/index.js
@@ -0,0 +1,12 @@
+import { Fragment } from "react/jsx-runtime";
+import MeetupDetails from "../../components/meetups/MeetupDetails";
+
+export default function MeetupDetails() {
+    return (<MeetupDetails
+        image="https://upload.wikimedia.org/wikipedia/commons/thumb/6/66/Louvre_Museum_Wikimedia_Commons.jpg/1920px-Louvre_Museum_Wikimedia_Commons.jpg"
+        title='The Firts Meetup at Le Louvre'
+        address='Some ramdom location, KISS CITY'
+        description='The meetup Description'
+    />);
+
+}
